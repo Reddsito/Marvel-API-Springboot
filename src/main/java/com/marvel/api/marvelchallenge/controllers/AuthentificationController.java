d6e898077979590396d3ddac7f525116bd8da309
Reddsito <ptyelnegro@gmail.com>
2023-11-13 20:31:45 -0500

    Se añadió: 1- Archivo de data autogenerada, además de configuración de las properties para que se añadan a la base de dato. 2- Archivo WORD con las indicaciones de la prueba técnica.

--- src/main/java/com/marvel/api/marvelchallenge/controllers/AuthentificationController.java
+++ src/main/java/com/marvel/api/marvelchallenge/controllers/AuthentificationController.java
@@ -6,6 +6,7 @@ import com.marvel.api.marvelchallenge.services.AuthenticationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class AuthentificationController {
 
     private final AuthenticationService authenticationService;
 
+    @PreAuthorize("permitAll()")
     @PostMapping("/authenticate")
     public ResponseEntity<LoginResponse> authenticate(
             @RequestBody @Valid LoginRequest loginRequest)
@@ -25,6 +27,7 @@ public class AuthentificationController {
     return ResponseEntity.ok(authenticationService.authenticate(loginRequest));
     }
 
+    @PreAuthorize("permitAll()")
     @PostMapping("/logout")
     public void logout() throws Exception {
         authenticationService.logout();
